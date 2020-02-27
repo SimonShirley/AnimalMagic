4daf9673d065b9523a9543c58a7d8a88ba1faf19
Simon Shirley <email@simonshirley.uk>
2020-02-27 23:50:59 +0000

    Added Entity Tests

---
+++ AnimalMagic.Test/EntityTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnimalMagic.Entity;
+
+namespace AnimalMagic.Test
+{
+    [TestClass]
+    public class EntityTests
+    {
+        [TestMethod]
+        public void isCatAMammalTest()
+        {
+            var cat = new Cat("Shanie");
+            Assert.IsInstanceOfType(cat, typeof(Mammal));
+        }
+
+        [TestMethod]
+        public void isCatAnAnimalTest()
+        {
+            var cat = new Cat("Shanie");
+            Assert.IsInstanceOfType(cat, typeof(Animal));
+        }
+
+        [TestMethod]
+        public void isCatABirdTest()
+        {
+            var cat = new Cat("Shanie");
+            Assert.IsNotInstanceOfType(cat, typeof(Bird));
+        }
+
+        [TestMethod]
+        public void isDogAMammalTest()
+        {
+            var dog = new Dog("Blazer", 4);
+            Assert.IsInstanceOfType(dog, typeof(Mammal));
+        }
+
+        [TestMethod]
+        public void isDogAnAnimalTest()
+        {
+            var dog = new Dog("Blazer", 3);
+            Assert.IsInstanceOfType(dog, typeof(Animal));
+        }
+
+        [TestMethod]
+        public void isDogABirdTest()
+        {
+            var dog = new Dog("Duke");
+            Assert.IsNotInstanceOfType(dog, typeof(Bird));
+        }
+
+        [TestMethod]
+        public void isParrotAnAnimal()
+        {
+            var parrot = new Parrot("Bluey");
+            Assert.IsInstanceOfType(parrot, typeof(Animal));
+        }
+
+        [TestMethod]
+        public void isParrotAMammal()
+        {
+            var parrot = new Parrot("Bluey");
+            Assert.IsNotInstanceOfType(parrot, typeof(Mammal));
+        }
+
+        [TestMethod]
+        public void isParrotABird()
+        {
+            var parrot = new Parrot("Bluey", 8);
+            Assert.IsInstanceOfType(parrot, typeof(Bird));
+        }
+    }
+}

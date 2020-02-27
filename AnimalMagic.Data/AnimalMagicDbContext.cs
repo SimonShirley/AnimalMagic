767dea65218ff3ca50378d62895741dce52506fb
Simon Shirley <email@simonshirley.uk>
2020-02-27 20:33:32 +0000

    Added InMemory Database DbContext

---
+++ AnimalMagic.Data/AnimalMagicDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalMagic.Data
+{
+    public class AnimalMagicDbContext : DbContext
+    {
+        private readonly DbContextOptions options;
+
+        public AnimalMagicDbContext(DbContextOptions<AnimalMagicDbContext> options)
+        {
+            this.options = options;
+        }
+    }
+}
--- AnimalMagic.Web/Startup.cs
+++ AnimalMagic.Web/Startup.cs
@@ -1,3 +1,5 @@
+using AnimalMagic.Data;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +27,9 @@ namespace AnimalMagic.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<AnimalMagicDbContext>(options =>
+                options.UseInMemoryDatabase("animalMagicDb")
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

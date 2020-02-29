c02c09778b2825293d6efe7f6f104b296cde0ec5
Simon Shirley <email@simonshirley.uk>
2020-02-29 23:51:43 +0000

    Added Cat and Parrot Add and List actions

---
+++ AnimalMagic.Web/Controllers/CatsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AnimalMagic.Business.Interfaces;
+using AnimalMagic.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalMagic.Web.Controllers
+{
+    public class CatsController : Controller
+    {
+        private readonly IAnimalManager _animalManager;
+
+        public CatsController(IAnimalManager animalManager)
+        {
+            _animalManager = animalManager;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction(nameof(List));
+        }
+
+        public IActionResult List()
+        {
+            List<Cat> dogCollection = _animalManager.Animals.Where(a => a is Cat).Cast<Cat>().OrderBy(c => c.Name).ToList();
+            return View(dogCollection);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Cat cat)
+        {
+            if (ModelState.IsValid)
+            {
+                _animalManager.Animals.Add(cat);
+                TempData["AnimalAdded"] = true;
+                return RedirectToAction(nameof(List));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ AnimalMagic.Web/Controllers/ParrotsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AnimalMagic.Business.Interfaces;
+using AnimalMagic.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalMagic.Web.Controllers
+{
+    public class ParrotsController : Controller
+    {
+        private readonly IAnimalManager _animalManager;
+
+        public ParrotsController(IAnimalManager animalManager)
+        {
+            _animalManager = animalManager;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction(nameof(List));
+        }
+
+        public IActionResult List()
+        {
+            List<Parrot> dogCollection = _animalManager.Animals.Where(a => a is Parrot).Cast<Parrot>().OrderBy(p => p.Name).ToList();
+            return View(dogCollection);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Parrot parrot)
+        {
+            if (ModelState.IsValid)
+            {
+                _animalManager.Animals.Add(parrot);
+                TempData["AnimalAdded"] = true;
+                return RedirectToAction(nameof(List));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
